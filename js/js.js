e8de15c1c63724c342662fbed74eef1d61487d92
marykaweb <marykaweb@gmail.com>
2017-12-23 19:21:15 +0300

    231217 1921

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
-    $('.menu').hover(function(){ // задаем функцию при наведении курсора на элемент
-        $('.navigation li').fadeIn('slow').css(
-            {
-                display: 'block'
-            }
-        );
-    }, function() { // задаем функцию, которая срабатывает, когда указатель выходит из элемента
-        $('.navigation li:not(.menu_icons)').css(
-            {
-                display: "none"
-             }
-        )
-    });
+    // $('.menu').hover(function(){ // задаем функцию при наведении курсора на элемент
+    //     $('.navigation li').fadeIn('slow').css(
+    //         {
+    //             display: 'block'
+    //         }
+    //     );
+    // }, function() { // задаем функцию, которая срабатывает, когда указатель выходит из элемента
+    //     $('.navigation li:not(.menu_icons)').css(
+    //         {
+    //             display: "none"
+    //          }
+    //     )
+    // });
